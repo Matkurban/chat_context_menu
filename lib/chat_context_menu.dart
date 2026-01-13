f415a44c1f3be1f55c8e241db0c2027301647695
matkurban <3496354336@qq.com>
2026-01-14 01:12:28 +0800

    release 1.5.0 version

--- lib/chat_context_menu.dart
+++ lib/chat_context_menu.dart
@@ -1,4 +1,6 @@
 library;
 
+export 'src/shape/chat_context_menu_horizontal_shape.dart';
+export 'src/shape/chat_context_menu_vertical_shape.dart';
 export 'src/ui/chat_context_menu_wrapper.dart';
 export 'src/model/statement.dart';
